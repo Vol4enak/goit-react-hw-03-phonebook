fba50e7e966d08abe331afb0f60737f4b3a371a4
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-03-13 18:01:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
   deleteContact = idContact => {
@@ -48,9 +43,23 @@ export class App extends Component {
       contact.name.toLocaleLowerCase().includes(normalaizedfilter)
     );
   };
+
+  componentDidMount(prevProps, prevState) {
+    const contact = localStorage.getItem("contacts")
+    const parsetCont = JSON.parse(contact);
+    this.setState({contacts: parsetCont})
+  }
+  componentDidUpdate = (prevProps, prevState) => {
+ 
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+  
+
   render() {
     const visibility = this.visibleContact();
-   
+
     return (
       <Container>
         <h1>Phonebook</h1>
